a634a090498813788ff15893118faf63a5ac2184
LabRat142 <skyguy142@gmail.com>
2023-12-03 14:48:33 +0100

    get json from resources

--- tech prototype/code/src/main/java/dians_project/storimak/repository/PlaceRepository.java
+++ tech prototype/code/src/main/java/dians_project/storimak/repository/PlaceRepository.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dians_project.storimak.model.Place;
 import dians_project.storimak.service.PlaceService;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Repository;
 
 import java.io.File;
@@ -17,15 +20,15 @@ public class PlaceRepository implements PlaceService {
     public static List<Place> placeList = new ArrayList<>();
 
     public PlaceRepository() throws IOException {
-        placeList = readJsonFromFile("C:\\Users\\Rat xd\\OneDrive\\Desktop\\UniOneDrive\\Semester 5\\Dians\\HWs\\Project\\hw 2\\code\\storimak\\src\\main\\resources\\static\\base.json");
+        placeList = readJsonFromFile("static/base.json");
     }
     @Override
     public List<Place> readJsonFromFile(String filePath) throws IOException {
+        Resource resource = new ClassPathResource(filePath);
         ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(new File(filePath), new TypeReference<List<Place>>() {});
+        return objectMapper.readValue(resource.getFile(), new TypeReference<List<Place>>() {});
         // Read JSON from file and convert to List<Place>
     }
-
     @Override
     public List<Place> findAll() {
         return placeList;
